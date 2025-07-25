ea4801d1e9a7265c8d68040bae33bf632d8bdea7
Krizel Minnema <krizelminnema@pm.me>
2025-07-25 09:47:53 -0700

    adding module exports back into gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,3 +26,23 @@ exports.createPages = async ({ graphql, actions }) => {
     });
   });
 };
+
+module.exports = {
+  siteMetadata: {
+    title: `Pretty Little Poppy Marketplace | Antelope Valley, CA`,
+    siteUrl: `https://jamm.matter.design`,
+  },
+  plugins: [ {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
+        host: process.env.GATSBY_CONTENTFUL_HOST, 
+      },
+    },
+    // other plugins...
+  ],
+  flags: {
+    THE_FLAG: false,
+  }
+};
