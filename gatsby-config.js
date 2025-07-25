31c83595b372e005e1f8b45bed71eb69eabcd167
Krizel Minnema <krizelminnema@pm.me>
2025-07-25 09:14:24 -0700

    remove .env files from version control

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,22 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Sydney Ecommerce Theme`,
+    title: `Pretty Little Poppy Marketplace | Antelope Valley, CA`,
     siteUrl: `https://jamm.matter.design`,
   },
-  plugins: [],
+  plugins: [ {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
+        host: process.env.GATSBY_CONTENTFUL_HOST, 
+      },
+    },
+    // other plugins...
+  ],
   flags: {
     THE_FLAG: false,
   }
