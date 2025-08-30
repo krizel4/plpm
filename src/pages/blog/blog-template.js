430fdd15ea3e3417999e967effacb572dce17894
Krizel Minnema <krizel@macbookair.local.ftr>
2025-08-29 23:04:49 -0700

    fixing text overlap in blog template

--- src/pages/blog/blog-template.js
+++ src/pages/blog/blog-template.js
@@ -1,73 +1,71 @@
-import React from 'react'
-import { navigate } from 'gatsby'
+import React from "react";
+import { navigate } from "gatsby";
 
-import BlogPreviewGrid from '../../components/BlogPreviewGrid'
-import Container from '../../components/Container'
-import Hero from '../../components/Hero'
-import Layout from '../../components/Layout/Layout'
-import ThemeLink from '../../components/ThemeLink'
+import BlogPreviewGrid from "../../components/BlogPreviewGrid";
+import Container from "../../components/Container";
+import Hero from "../../components/Hero";
+import Layout from "../../components/Layout/Layout";
+import ThemeLink from "../../components/ThemeLink";
 
-import { generateMockBlogData } from '../../helpers/mock'
-import * as styles from './index.module.css'
-import { toOptimizedImage } from '../../helpers/general'
+import { generateMockBlogData } from "../../helpers/mock";
+import * as styles from "./index.module.css";
+import { toOptimizedImage } from "../../helpers/general";
 
 const BlogPage = (props) => {
-  const blogData = generateMockBlogData(6)
+  const blogData = generateMockBlogData(6);
 
   return (
     <Layout disablePaddingBottom>
       <div className={styles.root}>
         <Hero
-          maxWidth={'400px'}
-          image={toOptimizedImage('/blogCover.png')}
-          title={'The new standard of Closing'}
-          ctaLink={'read story'}
-          ctaTo={'/blog/sample'}
-          header={'design'}
+          image={toOptimizedImage(
+            "https://images.ctfassets.net/xuyebjupxu9b/2VZ1I8yzTtHh12m8p2ZaTE/28790671434e620d75f49920c49d9e0c/Pretty-little-poppy-marketplace-Lancaster-ca-antelope-valley-enhanced.png"
+          )}
+          title={`Thrive Guide`}
+          subtitle={`Guidance, Growth and Community`}
         />
-
         <div className={styles.navContainer}>
           <ThemeLink
-            onClick={() => navigate('/blog/sample')}
-            to={'/blog/sample'}
+            onClick={() => navigate("/blog/sample")}
+            to={"/blog/sample"}
           >
             All Posts
           </ThemeLink>
           <ThemeLink
-            onClick={() => navigate('/blog/sample')}
-            to={'/blog/sample'}
+            onClick={() => navigate("/blog/sample")}
+            to={"/blog/sample"}
           >
-            Design
+            Business
           </ThemeLink>
           <ThemeLink
-            onClick={() => navigate('/blog/sample')}
-            to={'/blog/sample'}
+            onClick={() => navigate("/blog/sample")}
+            to={"/blog/sample"}
           >
-            Collaboration
+            Marketing
           </ThemeLink>
           <ThemeLink
-            onClick={() => navigate('/blog/sample')}
-            to={'/blog/sample'}
+            onClick={() => navigate("/blog/sample")}
+            to={"/blog/sample"}
           >
-            Interview
+            AV Life
           </ThemeLink>
           <ThemeLink
-            onClick={() => navigate('/blog/sample')}
-            to={'/blog/sample'}
+            onClick={() => navigate("/blog/sample")}
+            to={"/blog/sample"}
           >
-            News
+            Minors to Majors
           </ThemeLink>
         </div>
 
         {/* Blog Grid */}
         <div className={styles.blogsContainer}>
-          <Container size={'large'}>
+          <Container size={"large"}>
             <BlogPreviewGrid data={blogData} hideReadMoreOnWeb showExcerpt />
           </Container>
         </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default BlogPage
+export default BlogPage;
